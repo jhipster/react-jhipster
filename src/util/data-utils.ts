fc00c7b6433230bdbc7dffb82eeb61a37b5fec76
Alexandre GC <agaspard@ippon.fr>
2018-03-21 11:32:12 +0100

    add some tests for data-utils

--- src/util/data-utils.ts
+++ src/util/data-utils.ts
@@ -1,3 +1,17 @@
+/*
+ Copyright 2013-2017 the original author or authors from the JHipster project.
+ This file is part of the JHipster project, see https://jhipster.github.io/
+ for more information.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 export const openFile = (contentType: string, data: string) => () => {
   const fileURL = `data:${contentType};base64,${data}`;
   const win = window.open();
@@ -17,7 +31,7 @@ const toBase64 = (file: File, cb: Function) => {
   };
 };
 
-export const paddingSize = (value: string): number => {
+const paddingSize = (value: string): number => {
   if (value.endsWith('==')) {
     return 2;
   }
@@ -27,7 +41,7 @@ export const paddingSize = (value: string): number => {
   return 0;
 };
 
-export const formatAsBytes = (sizeValue: number): string => sizeValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ') + ' bytes';
+const formatAsBytes = (sizeValue: number): string => sizeValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ') + ' bytes';
 
 export const size = (value: string): number => value.length / 4 * 3 - paddingSize(value);
 
