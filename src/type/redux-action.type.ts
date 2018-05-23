d9b517825f738b2f6a271371ce275bdfeadb1e88
Deepu K Sasidharan <d4udts@gmail.com>
2018-05-23 10:14:18 +0530

    update redux-action types

--- src/type/redux-action.type.ts
+++ src/type/redux-action.type.ts
@@ -1,11 +1,13 @@
+import { AxiosPromise } from 'axios';
+
 export interface IPayload<T> {
   type: string;
-  payload: Promise<T>;
+  payload: AxiosPromise<T>;
   meta?: any;
 }
 export type IPayloadResult<T> = ((dispatch: any) => IPayload<T> | Promise<IPayload<T>>);
 export type ICrudGetAction<T> = (id: string | number) => IPayload<T> | ((dispatch: any) => IPayload<T>);
 export type ICrudGetAllAction<T> = (page?: number, size?: number, sort?: string) => IPayload<T> | ((dispatch: any) => IPayload<T>);
 export type ICrudSearchAction<T> = (search?: string) => IPayload<T> | ((dispatch: any) => IPayload<T>);
-export type ICrudDeleteAction<T> = (id?: string | number) => IPayload<T> | IPayloadResult<T>;
 export type ICrudPutAction<T> = (data?: T) => IPayload<T> | IPayloadResult<T>;
+export type ICrudDeleteAction<T> = (id?: string | number) => IPayload<T> | IPayloadResult<T>;
