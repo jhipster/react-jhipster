ed05bbda1d280ba7d246ed944e335fbaec5b1b73
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-11-07 18:31:49 -0800

    add pagination params to ICrudSearchAction
    related to jhipster/generator-jhipster#8776

--- src/type/redux-action.type.ts
+++ src/type/redux-action.type.ts
@@ -8,6 +8,6 @@ export interface IPayload<T> {
 export type IPayloadResult<T> = ((dispatch: any) => IPayload<T> | Promise<IPayload<T>>);
 export type ICrudGetAction<T> = (id: string | number) => IPayload<T> | ((dispatch: any) => IPayload<T>);
 export type ICrudGetAllAction<T> = (page?: number, size?: number, sort?: string) => IPayload<T> | ((dispatch: any) => IPayload<T>);
-export type ICrudSearchAction<T> = (search?: string) => IPayload<T> | ((dispatch: any) => IPayload<T>);
+export type ICrudSearchAction<T> = (search?: string, page?: number, size?: number, sort?: string) => IPayload<T> | ((dispatch: any) => IPayload<T>);
 export type ICrudPutAction<T> = (data?: T) => IPayload<T> | IPayloadResult<T>;
 export type ICrudDeleteAction<T> = (id?: string | number) => IPayload<T> | IPayloadResult<T>;
