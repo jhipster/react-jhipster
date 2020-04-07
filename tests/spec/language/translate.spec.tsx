1bb5b232ea35bb48d922330575419b4a471a36b1
Sonia Lahcene <sonia.lahcene.89@gmail.com>
2020-04-07 14:36:16 +0200

    fix translations for falsy values : empty strings, number 0 and boolean false

--- tests/spec/language/translate.spec.tsx
+++ tests/spec/language/translate.spec.tsx
@@ -33,6 +33,11 @@ describe('Translate', () => {
             'bar8.bar9': {
               'bar10.bar11': 'bar7891011'
             }
+          },
+          barfalsy: {
+            'empty': '',
+            'zero': 0,
+            'false': false,
           }
         }
       });
@@ -114,28 +119,28 @@ describe('Translate', () => {
       expect(span.html()).to.equal('<span>bar7891011</span>');
       expect(span.text()).to.equal('bar7891011');
     });
-    it('renders a provided componenet with translated content', () => {
+    it('renders a provided component with translated content', () => {
       const mountedWrapper = mount(<Translate contentKey="foo.bar" component="h1" />);
       const span = mountedWrapper.find('h1');
       expect(span.length).to.equal(1);
       expect(span.html()).to.equal('<h1>i18n text</h1>');
       expect(span.text()).to.equal('i18n text');
     });
-    it('renders a provided componenet with translated & interpolated content', () => {
+    it('renders a provided component with translated & interpolated content', () => {
       const mountedWrapper = mount(<Translate contentKey="foo.fooz" component="h1" interpolate={{ foo: 'FOO', bar: 'BAR' }} />);
       const span = mountedWrapper.find('h1');
       expect(span.length).to.equal(1);
       expect(span.html()).to.equal('<h1>text FOO this BAR</h1>');
       expect(span.text()).to.equal('text FOO this BAR');
     });
-    it('renders a provided componenet with translated & interpolated content with html', () => {
+    it('renders a provided component with translated & interpolated content with html', () => {
       const mountedWrapper = mount(<Translate contentKey="foo.foofoo" component="h1" interpolate={{ foo: 'FOO', bar: 'BAR' }} />);
       const span = mountedWrapper.find('h1');
       expect(span.length).to.equal(1);
       expect(span.html()).to.equal('<h1>text FOO this BAR <b>test</b></h1>');
       expect(span.text()).to.equal('text FOO this BAR test');
     });
-    it('renders a provided componenet with translated, sanitized & interpolated content', () => {
+    it('renders a provided component with translated, sanitized & interpolated content', () => {
       const mountedWrapper = mount(<Translate contentKey="foo.foodirty" component="h1" interpolate={{ foo: 'FOO', bar: 'BAR' }} />);
       const span = mountedWrapper.find('h1');
       expect(span.length).to.equal(1);
@@ -149,6 +154,30 @@ describe('Translate', () => {
       expect(span.length).to.equal(1);
       expect(span.html()).to.equal('<span>i18n text fr</span>');
       expect(span.text()).to.equal('i18n text fr');
+    });
+
+    it('renders a default span with translated content for empty string', () => {
+      const mountedWrapper = mount(<Translate contentKey="foo.barfalsy.empty" />);
+      const span = mountedWrapper.find('span');
+      expect(span.length).to.equal(1);
+      expect(span.html()).to.equal('<span></span>');
+      expect(span.text()).to.equal('');
+    });
+
+    it('renders a default span with translated content for number 0', () => {
+      const mountedWrapper = mount(<Translate contentKey="foo.barfalsy.zero" />);
+      const span = mountedWrapper.find('span');
+      expect(span.length).to.equal(1);
+      expect(span.html()).to.equal('<span>0</span>');
+      expect(span.text()).to.equal('0');
+    });
+
+    it('renders a default span with translated content for boolean false', () => {
+      const mountedWrapper = mount(<Translate contentKey="foo.barfalsy.false" />);
+      const span = mountedWrapper.find('span');
+      expect(span.length).to.equal(1);
+      expect(span.html()).to.equal('<span>false</span>');
+      expect(span.text()).to.equal('false');
     });
   });
 });
