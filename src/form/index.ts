d81fb9cb4adfe5669ae118219333411dcb38ee1c
deepu105 <d4udts@gmail.com>
2021-06-02 17:58:09 +0200

    fix export

--- src/form/index.ts
+++ src/form/index.ts
@@ -1,15 +1,1 @@
-import {
-  ValidatedForm,
-  ValidatedField,
-  ValidatedInput,
-  ValidatedFieldProps,
-  ValidatedFormProps,
-  ValidatedInputProps,
-} from './validated-form';
-
-export { ValidatedForm };
-export { ValidatedField };
-export { ValidatedInput };
-export { ValidatedFieldProps };
-export { ValidatedFormProps };
-export { ValidatedInputProps };
+export * from './validated-form';
