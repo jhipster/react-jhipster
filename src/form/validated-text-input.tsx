12ed8baa28f6ee1b61e7efcb5d09c08bfeae0c10
Charlie Mordant <cmordant1@gmail.com>
2022-09-11 21:40:06 +0200

    use react forms attributes

--- src/form/validated-text-input.tsx
+++ src/form/validated-text-input.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FieldValues, UseFormRegister, UseFormSetValue } from "react-hook-form";
+import { FieldValues, RegisterOptions, UseFormRegister, UseFormSetValue } from "react-hook-form";
 import { FormFeedback, FormGroup, Input, Label } from "reactstrap";
 import { InputType } from "reactstrap/types/lib/Input";
 import { translate } from "../language";
@@ -9,17 +9,13 @@ export interface ValidatedTextInputFieldProps {
   register:  UseFormRegister<FieldValues>,
   touchedFields: any, errors,
   setValue: UseFormSetValue<FieldValues>,
-  name: string,
-  validate: {
-    required?: boolean | string,
-    minLength?: { value: number, message: string },
-    maxLength?: { value: number, message: string },
-    pattern?: { value: RegExp, message: string },
-    validate?: (value: string) => boolean | string
-  },
+  nameIdCy: string,
+  validate: RegisterOptions<FieldValues>,
   labelPlaceholderKey: string,
-  inputPlaceholderKey: string,
+  inputPlaceholderKey?: string,
   type?: InputType,
+  readOnly?: boolean,
+  disabled?: boolean,
   updateValueOverrideMethod?: (event: any) => void,
 }
 export function ValidatedTextInput(
@@ -28,39 +24,43 @@ export function ValidatedTextInput(
     touchedFields,
     errors,
     setValue,
-    name,
+    nameIdCy,
     validate,
     labelPlaceholderKey,
-    inputPlaceholderKey,
+    inputPlaceholderKey = '',
     type = 'text',
+    readOnly = false,
+    disabled = false,
     updateValueOverrideMethod,
   }: ValidatedTextInputFieldProps) {
 
   const updateValueDefaultMethod = event => {
-    setValue(name, event.target.value, { shouldValidate: true, shouldDirty: true, shouldTouch: true });
+    setValue(nameIdCy, event.target.value, { shouldValidate: true, shouldDirty: true, shouldTouch: true });
   };
 
   const updateValue = updateValueOverrideMethod?? updateValueDefaultMethod;
-
-  const labelName = `${name}Label`;
+  const placeHolderValue = inputPlaceholderKey ? translate(inputPlaceholderKey) : '';
+  const labelName = `${nameIdCy}Label`;
 
   return (
     <FormGroup>
-      <Label id={labelName} for={name}>
+      <Label id={labelName} for={nameIdCy}>
         {translate(labelPlaceholderKey)}
       </Label>
       <Input
-        id={name}
-        name={name}
-        placeholder={ translate(inputPlaceholderKey) }
+        id={nameIdCy}
+        name={nameIdCy}
+        placeholder={ placeHolderValue }
         type={type}
-        {...register(name, validate)}
-        data-cy={name}
-        valid={touchedFields[name] && !errors[name]}
-        invalid={!touchedFields[name] || !!errors[name]}
+        readOnly={readOnly}
+        disabled={disabled}
+        {...register(nameIdCy, validate)}
+        data-cy={nameIdCy}
+        valid={touchedFields[nameIdCy] && !errors[nameIdCy]}
+        invalid={!touchedFields[nameIdCy] || !!errors[nameIdCy]}
         onChange={updateValue}
       />
-      <FormFeedback hidden={!errors[name]}>{errors[name]?.message}</FormFeedback>
+      <FormFeedback hidden={!errors[nameIdCy]}>{errors[nameIdCy]?.message}</FormFeedback>
     </FormGroup>
   );
 }
